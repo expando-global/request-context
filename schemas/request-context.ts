9345c1343d56e46cf8e13fb4b59b61cd93de7eea
Radek Kysely <radek@kysely.org>
2020-06-23 12:08:50 +0200

    (change) Interface

--- schemas/request-context.ts
+++ schemas/request-context.ts
@@ -2,9 +2,15 @@ import * as Joi from 'typesafe-joi';
 import { ObjectId } from 'mongodb';
 
 export const RequestContextSchema = Joi.object({
-    companyId: Joi.object().type(ObjectId),
+    companyId: Joi.object().type(ObjectId).required(),
     ip: Joi.string().required(),
     endpoint: Joi.string().required(),
     token: Joi.string().required(),
 }).required();
-export type IRequestContext = Joi.Literal<typeof RequestContextSchema>;
+
+export interface IRequestContext {
+    companyId: ObjectId;
+    ip: string;
+    endpoint: string;
+    token: string;
+}
